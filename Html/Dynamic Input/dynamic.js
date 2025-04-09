74aab07c6be6ec9a45fc887629ea79e2641a2674
JayaSriRaman2003 <jayasriraman.learning@gmail.com>
2025-04-09 09:24:49 +0530

    Commit All

---
+++ Html/Dynamic Input/dynamic.js
@@ -0,0 +1,19 @@
+
+
+function Register() {
+
+    
+const arr = {
+    emailNew: "",
+    passNew : ""
+}
+        const email = document.getElementById("email-value").value;
+        const pass = document.getElementById("password-value").value;
+
+        email = emailNew;
+        pass = passNew;
+    
+        console.log(email);
+        console.log(pass);
+        console.log(arr)
+    } 
